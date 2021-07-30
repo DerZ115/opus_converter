322f7c90217926fcf204d194ba38880301ade24b
Daniel Zimmermann <daniel.zimmermann.dz@gmail.com>
2021-07-30 12:05:11 +0200

    removed dry run

--- rename_files.py
+++ rename_files.py
@@ -1,7 +1,7 @@
 import os
 import re
 
-data_path = "C:/Users/Daniel/Desktop/Raman_script/spectra/Behandlungsversuche_neu/E2_txt"
+data_path = r"C:\Users\Daniel\Desktop\Raman_script\spectra\HL_Behandlungsversuche\Etoposid2"
 group_name = "E2"
 
 files = os.listdir(data_path)
@@ -14,7 +14,7 @@ files_new = []
 
 for i in range(n):
     new_filename = group_name + "_" + str(i+1).zfill(digits) + ".TXT"
-    # os.rename(os.path.join(data_path, files[i]), os.path.join(data_path, new_filename))
+    os.rename(os.path.join(data_path, files[i]), os.path.join(data_path, new_filename))
     print(f"{i}: {files[i]} -> {new_filename}")
     
 
